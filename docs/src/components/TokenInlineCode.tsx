9f2446f990eb607a179a9575b50a1eef78109d9b
诸葛林 <ktnig@qq.com>
2023-11-24 01:58:03 +0800

    Revert "Remove unused Gatsby site (#658)"
    This reverts commit d7bf1048ef100ea3d448827f65b824fff1f44cd3.

---
+++ docs/src/components/TokenInlineCode.tsx
@@ -0,0 +1,9 @@
+import InlineCode from '@primer/gatsby-theme-doctocat/src/components/inline-code'
+import styled from 'styled-components'
+
+const TokenInlineCode = styled(InlineCode)`
+  white-space: nowrap;
+  user-select: all;
+`
+
+export default TokenInlineCode
---
+++ docs/src/components/TokenTable.tsx
@@ -0,0 +1,8 @@
+import Table from '@primer/gatsby-theme-doctocat/src/components/table'
+import styled from 'styled-components'
+
+const TokenTable = styled(Table)`
+  display: table;
+`
+
+export default TokenTable
